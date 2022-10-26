17762dec9152c12e03540ab33ff3e85956204959
Stefan Voicila <2186398@cognizant.com>
2022-10-27 02:50:11 +0300

    https

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'taci';
+  location: Location;
+
+  ngOnInit(): void {
+    if (location.protocol === 'http:'){
+      window.location.href = location.href.replace('http', 'https');
+    }
+  }
 }
