c4118c589130640c340d41ce4f4152778e496bc0
Stefan Voicila <2186398@cognizant.com>
2022-10-27 03:37:02 +0300

    https try 83

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const enforce = require('express-sslify');
 
 const app = express();
+
+app.use(enforce.HTTPS());
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/taci'));
